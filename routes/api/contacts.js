6fe74808f8e639f2be288983067e444631810ce6
ExLuZiVe53 <imstudent27@gmail.com>
2023-12-11 19:17:12 +0100

    added all files

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+<<<<<<< Updated upstream
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
@@ -9,11 +10,19 @@ router.get('/', async (req, res, next) => {
 router.get('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
+=======
+const ctrl = require("../../controllers/contacts");
+
+const { validateBody, isValidId } = require("../../midlewares");
+
+const { addSchema, updateFavotiteSchema } = require("../../models/contact");
+>>>>>>> Stashed changes
 
 router.post('/', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
+<<<<<<< Updated upstream
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
@@ -23,3 +32,18 @@ router.put('/:contactId', async (req, res, next) => {
 })
 
 module.exports = router
+=======
+router.get("/", ctrl.getAll);
+
+router.get("/:id", isValidId, ctrl.getById);
+
+router.post("/", validateBody(addSchema), ctrl.add);
+
+router.put("/:id", isValidId, validateBody(addSchema), ctrl.updateById);
+
+router.patch("/:id/favorite", isValidId, validateBody(updateFavotiteSchema), ctrl.updateFavorite);
+
+router.delete("/:id", isValidId, ctrl.deleteById);
+
+module.exports = router;
+>>>>>>> Stashed changes
